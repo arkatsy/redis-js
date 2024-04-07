21b3bbb1abbf1fd31e676ab8a92209e6bed340a3
Argyris <arkatsy@gmail.com>
2024-04-07 15:21:49 +0300

    pass stage

--- app/main.js
+++ app/main.js
@@ -1,7 +1,13 @@
 const net = require("node:net");
+const port = 6379;
+const host = "127.0.0.1";
 
-const server = net.createServer((connection) => {
-    
+const server = net.createServer((socket) => {
+  socket.on("data", (data) => {
+    socket.write("+PONG\r\n");
+  });
 });
 
-server.listen(6379, "127.0.0.1");
+server.listen(port, host, () => {
+  console.log(`[LOG] Server is running on port ${port}`);
+});
